4cbcdddf4c4cd80b7db3119cffd8156a511d4a9a
Abderrahmen Gharsallah <aa.test20@gmail.com>
2017-03-03 01:00:09 +0100

    added city info search

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,13 @@ const cors = require('cors');
 const moment = require('moment');
 const util = require('util');
 const _ = require('lodash');
+var natural = require('natural');
 
 const {generateMessage} = require('./utils/message');
 const {Users} = require('./users');
 
 const general_arr = require("./utils/message_src/greeting")
+const all_mun = require("./utils/message_src/all_mun")
 
 var users = new Users();
 
@@ -117,18 +119,38 @@ io.on('connection', (socket) => {
 	//print the user's message
 	socket.emit('server:newMessage', message);
 	/*Check if the users question matches a predefined Q*/
-	var grtng= general_arr.greeting.indexOf(message.body)
-	var grtng_resp = general_arr.greeting_response
+	var grtng= general_arr.greeting.indexOf(message.body);
+	var grtng_resp = general_arr.greeting_response;
+	var verif = []
+	//_.findIndex(all_mun.features) 
+		all_mun.features.forEach( function (arrayItem)
+		{
+			var city_name = arrayItem.properties.name_en;
+			var city_pop = arrayItem.properties.citizens;
+			var city_area = arrayItem.properties.area;
+			var city_seats = arrayItem.properties.seats;
+			var closness = natural.JaroWinklerDistance(city_name,message.body)
+		    
+		    if (closness > 0.98) {
+					socket.emit('server:newMessage', generateMessage("Admin",`in ${city_name} Lives arround ${city_pop} people`));
+					console.log(arrayItem.properties.citizens)
+					
+		    }else{
+		    	mun_exist =0;
+		    }
+		});
+		
 	if(grtng!=-1){
-	var rand_reply = grtng_resp[Math.floor(Math.random()*grtng_resp.length)];
-	
-	setTimeout(()=>{
-		socket.emit('server:newMessage', generateMessage("Admin",rand_reply));
-	}, 400);
+		var rand_reply = grtng_resp[Math.floor(Math.random()*grtng_resp.length)];
+		setTimeout(()=>{
+			socket.emit('server:newMessage', generateMessage("Admin",rand_reply));
+		}, 400);
+		
 	}else if(message.body.toLowerCase().indexOf("info") >= 0) {
 		setTimeout(()=>{
 			socket.emit('server:newMessage', generateMessage("Admin","ok great in which municipality do u leave ?"));
 		}, 400);
+	
 	}else{
 		setTimeout(()=>{
 		socket.emit('server:newMessage', generateMessage("Admin","Sorry I didn't get what you get You can type help"));
