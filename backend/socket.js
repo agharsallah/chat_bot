7b012d2484e158c21bf0e556e8ee66ace6d7f139
Abderrahmen Gharsallah <aa.test20@gmail.com>
2017-03-05 11:46:04 +0100

    Revert "update"
    This reverts commit 3425cf60a8d6413467f0867982925c910c91e24c.

--- backend/socket.js
+++ backend/socket.js
@@ -9,31 +9,46 @@ const moment = require('moment');
 const util = require('util');
 var natural = require('natural');
 var simple_msg = require('./SocketEventHandlers/client_simple_msg')
-const say = require('say');
-
 module.exports = function(io,Message){
 
 io.on('connection', (socket) => {
+
+    socket.on('client:joinRoom', (channel, callback) => {
+	/* Do the socket magic that allows to send messages only to people in the room */
+	socket.join(channel);
+	/* Get all the messages from this channel */
+	Message.find({channel:channel}).then((messages)=>{
+	    console.log(`All messages on the ${channel} channel: ${messages} `);
+	    callback(messages);
+	});
+
+	//socket.leave(channel);
+    });    
+
     /* Join a room */
     socket.on('join', (params, callback) => {
 	/* Make sure the user isn't already on this channel */
 	users.removeUser(socket.id);
 	/* Add user to the channel (to the users object) */
 	users.addUser(socket.id, params.username, params.channel);
-	console.log(`${params.username} joined conversation`);
+	console.log(`${params.username} joined ${params.channel}`);
+
+	/* Emit an event to people in this room,
+	   telling them to update the list of users,
+	   and sending them the new user list. */
+	io.to(params.channel).emit('server:updateUserList',
+				   users.getUserList(params.channel));
 	/*add a hello message*/
 		socket.emit('server:first_hello',generateMessage("Admin",`Welcome ${params.username} I'm Your Election assistant`));
-    say.speak('hi there');
-
+	callback();
     });
     
-    /* listening to the event. Receivemessages from user. */
+        /* listening to the event. Receivemessages from user. */
     socket.on('client:createMessage', (message, callback) => {
     	//send the socket/ message I got from socket,and the Message model from db
     	var simple = simple_msg(socket,message,Message)
     	simple.emit();
     });
-    
     /* Disconnect */
     socket.on('disconnect', () => {
 	var user = users.removeUser(socket.id);
